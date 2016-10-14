c3512873c9560a59290d3a768fc7a71b47aafe1a
Sachin Ranchod <sachin.ranchod@gmail.com>
2016-10-14 16:28:09 -0700

    add status check

--- flows/chatter.js
+++ flows/chatter.js
@@ -168,7 +168,9 @@ module.exports = (slapp) => {
         }, {
             fallback: "buttons",
             color: "#fff",
+            callback_id: 'update_order_status_callback',
             actions: [
+               { "name": "update", "text": "Update status", "type": "button" },
                { "name": "cancel", "text": "Cancel", "style": "danger", "type": "button" }
             ]
         }
@@ -184,6 +186,47 @@ module.exports = (slapp) => {
         msg.respond('Sorry, I can not write to a channel or group I am not a part of!')
       }
     })
+  })
+
+  slapp.action('update_order_status_callback', 'update', (msg, value) => {
+    let original = msg.body.original_message;
+
+    original.attachments = [{
+            color: "#fff",
+            fallback: "image",
+            pretext: "Your order for 8 cups of Nepresso has been submitted. Your coffee is now brewing and will be delivered to shortly."
+        }, {
+            color: "#ab27b",
+            author_name: "1 cup of Ristretto",
+            author_link: "https://www.nespresso.com/us/en/order/capsules/original/professional-ristretto",
+            author_icon: "https://www.nespresso.com/ecom/medias/sys_master/public/9375746555934/C-0023-small-60x60.png"
+        }, {
+
+            color: "#ab27b",
+            author_name: "4 cups of Fortissio Lungo",
+            author_link: "https://www.nespresso.com/us/en/order/capsules/original/fortissio-lungo",
+            author_icon: "https://www.nespresso.com/ecom/medias/sys_master/public/9381719375902/C-0126-icon-42x42.png"
+        }, {
+            color: "#ab27b",
+            author_name: "3 cups of Vivalto Lungo",
+            author_link: "https://www.nespresso.com/us/en/order/capsules/original/vivalto-lungo",
+            author_icon: "https://www.nespresso.com/ecom/medias/sys_master/public/9375747997726/C-0038-icon-42x42.png"
+        }, {
+            fallback: "buttons",
+            color: "#fff",
+            callback_id: 'update_order_status_callback',
+            actions: [
+               { "name": "update", "text": "Update status", "type": "button" },
+               { "name": "cancel", "text": "Cancel", "style": "danger", "type": "button" }
+            ]
+        }
+    ]
+
+    msg.respond(msg.body.response_url, original)
+  })
+
+  slapp.action('update_order_status_callback', 'cancel', (msg, value) => {
+    msg.respond("Order cancelled")
   })
 
 }
