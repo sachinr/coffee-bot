03aafe60587abee48911212f8f9d37fc3bf692a0
Sachin Ranchod <sachin.ranchod@gmail.com>
2016-10-14 14:19:24 -0700

    add slash command

--- flows/chatter.js
+++ flows/chatter.js
@@ -5,35 +5,40 @@ const handleHowAreYou = 'chatter:handleHowAreYou'
 module.exports = (slapp) => {
 
   slapp.message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
-    msg
-      .say(text + ', how are you?')
-      .route(handleHowAreYou, {}, 60)
+    msg.say(text + ', how are you?')
   })
 
-  slapp.route(handleHowAreYou, (msg) => {
-    var resp = msg.body.event && msg.body.event.text
+  slapp.command('/start-demo', /.*/, (msg, text) => {
+    attachments.push({
+      text: '',
+      fallback: 'move to the bottom',
+      callback_id: 'in_or_out_callback',
+      actions: bottomActions
+    })
 
-    if (new RegExp('good', 'i').test(resp)) {
-      msg
-        .say(['Great! Ready?', ':smile: Are you sure?'])
-        .route(handleHowAreYou, 60)
-    } else {
-      msg.say('Me too')
+    msg.say({
+      text: text,
+      attachments: attachments
+    }, (err) => {
+      if (err && err.message === 'channel_not_found') {
+        msg.respond('Sorry, I can not write to a channel or group I am not a part of!')
+      }
+    })
+
+  })
+
+  slapp.action('in_or_out_callback', 'recycle', (msg, value) => {
+    var orig = msg.body.original_message
+    var update = {
+      text: 'In or out (moved to bottom): ' + orig.text,
+      delete_original: true
     }
-  })
-
-  slapp.message('^(thanks|thank you)', ['mention', 'direct_message'], (msg) => {
-    msg.say(['You are welcome', 'Of course'])
-  })
-
-  slapp.message('good night|bye', ['mention', 'direct_message'], (msg) => {
-    msg.say(['Cheers :beers:', 'Bye', 'Goodbye', 'Adios'])
-  })
-
-  slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
-    // respond only 40% of the time
-    if (Math.random() < 0.4) {
-      msg.say([':wave:', ':pray:', ':raised_hands:'])
-    }
+    msg.respond(update, (err) => {
+      if (err) return handleError(err, msg)
+      msg.say({
+        text: orig.text,
+        attachments: orig.attachments
+      })
+    })
   })
 }
